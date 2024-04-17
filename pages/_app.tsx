7971ba5da6d8237f19cc98ad4fb54f27da464bfd
fedev <federicoloterstein@gmail.com>
2024-04-16 22:15:11 -0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import { WalletProvider } from "@/context/WalletContext";
+import GoogleAnalytics from "@/lib/utils/GoogleAnalytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WalletProvider>
       <ChakraProvider>
+      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+            <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+          ) : null}
         <Container>
           <Component {...pageProps} />
         </Container>
