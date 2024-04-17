ac0ab401d2fdd456cb0edef9fcf6d3be41e173a4
fedev <federicoloterstein@gmail.com>
2024-04-16 22:02:50 -0300

    refactor

--- components/WalletHome.tsx
+++ components/WalletHome.tsx
@@ -35,13 +35,15 @@ export const WalletHome = () => {
   };
 
   return (
-    <Stack h={"100vh"} w={"100%"} justify={"flex-start"} align={"center"}>
+    <Stack h={"100vh"} w={"100%"} justify={"flex-start"} align={"center"} gap={10}>
       <Navbar deleteWallet={deleteWallet} />
+      <Stack  w={"100%"} align={"center"} >
       {balance?.increase > 0 && (
         <Text color={"green"}>+ {balance?.increase?.toFixed(5)} </Text>
       )}
       {balance && <Text fontSize="6xl">{balance?.balance?.toFixed(4)} ETH</Text>}
       <Menu  />
+      </Stack>
       <Tokens />
     </Stack>
   );
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,17 +1,21 @@
-import { convertFormat } from "@/lib/utils/convertFormat";
-import { HStack, IconButton, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, IconButton, Select, useColorMode } from "@chakra-ui/react";
 import React from "react";
 import { CgLogOut, CgSun, CgMoon } from "react-icons/cg";
 
 export const Navbar = ({ deleteWallet }: any) => {
+  const { colorMode, toggleColorMode } = useColorMode();
 
-  const { colorMode, toggleColorMode } = useColorMode()
-
-  const icon = colorMode === 'light' ? <CgSun />: <CgMoon />
+  const icon = colorMode === "light" ? <CgSun /> : <CgMoon />;
   return (
-    <HStack p={2} w={"100%"} justify={"flex-end"}>
-      <IconButton aria-label="" icon={<CgLogOut />} onClick={deleteWallet} />
-      <IconButton aria-label="" icon={icon} onClick={toggleColorMode}/>
+    <HStack p={2} w={"100%"} justify={'space-between'}>
+      <Select w={"fit-content"}>
+        <option value="option1">Morph Testnet</option>
+        <option disabled value="option2">Morph Mainnet</option>
+      </Select>
+      <HStack>
+        <IconButton aria-label="" icon={<CgLogOut />} onClick={deleteWallet} />
+        <IconButton aria-label="" icon={icon} onClick={toggleColorMode} />
+      </HStack>
     </HStack>
   );
 };
